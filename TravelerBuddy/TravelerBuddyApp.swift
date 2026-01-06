77b6d8716fe50dfac1b02ae3cd6d4b97e690c5ba
rehamsaad <rehamsd2@gmail.com>
2026-01-07 00:35:46 +0200

    Initial Commit

---
+++ TravelerBuddy/TravelerBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelerBuddyApp.swift
+//  TravelerBuddy
+//
+//  Created by Reham on 07/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct TravelerBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
